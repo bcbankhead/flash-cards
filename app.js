13c3f621d81ce2e86c77443d553a63c1a6d8c888
barrybankhead <bcbankhead@gmail.com>
2015-07-31 16:35:34 -0600

    pre-anna

--- app.js
+++ app.js
@@ -6,13 +6,25 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var authRoutes = require('./routes/auth');
 var users = require('./routes/users');
+
+var passport = require('passport');
+var SlackStrategy = require('passport-slack').Strategy
+
+var session = require('cookie-session');
+require('dotenv').load()
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(session({
+  name: 'session',
+  keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2]
+}))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +34,64 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new SlackStrategy({
+   clientID: process.env.SLACK_CLIENT_ID,
+   clientSecret: process.env.SLACK_CLIENT_SECRET,
+   callbackURL: process.env.CALLBACK_URL
+ },
+//  function(accessToken, refreshToken, profile, done) {
+//    User.findOrCreate({ SlackId: profile.id }, function (err, user) {
+//        , function (err, user) {
+//      return done(err, user);
+//    });
+//  }
+// ));
+
+  function(accessToken, refreshToken, profile, done) {
+    done(null, { SlackId: profile.id })
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user)
+});
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user
+  next()
+})
+
+app.get('/auth/slack',
+    passport.authorize('slack'));
+
+app.get('/auth/slack/callback',
+  passport.authorize('slack', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.render('dummy');
+  });
+
+// app.use(function (req,res,next) {
+//   if(req.isAuthenticated()){
+//     console.log(req.isAuthenticated());
+//     next()
+//   } else {
+//     console.log("not Authed");
+//     res.redirect('/auth/slack');
+//   }
+// })
+
 app.use('/', routes);
+app.use('/', authRoutes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', passport.authenticate('slack', { failureRedirect : '/auth/slack/callback'}), function(req, res, next) {
+  console.log(req.cookies.user);
+  res.render('index', {title: 'title'});
 });
 
 module.exports = router;
