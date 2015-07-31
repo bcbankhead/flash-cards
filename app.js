4fb78067f76fc8f59541fc5bb8df072bcee1dba4
Anna Casper <anna.casper3@gmail.com>
2015-07-31 15:41:51 -0600

    user can add new category

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('dotenv').load()
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var cards = require('./routes/cards');
 var app = express();
 
 // view engine setup
@@ -24,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/cards', cards);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
