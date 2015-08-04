98d293ca7e54762126990455e69adfeeefa37637
Judy Lin <judith.s.lin@gmail.com>
2015-08-04 09:14:15 -0600

    hint

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,13 +1,13 @@
-(function() {
-  var cx = '011127767182915430135:pcg-i_rg49w';
-  var gcse = document.createElement('script');
-  gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-      '//cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(gcse, s);
-})();
+// (function() {
+//   var cx = '011127767182915430135:pcg-i_rg49w';
+//   var gcse = document.createElement('script');
+//   gcse.type = 'text/javascript';
+//   gcse.async = true;
+//   gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+//       '//cse.google.com/cse.js?cx=' + cx;
+//   var s = document.getElementsByTagName('script')[0];
+//   s.parentNode.insertBefore(gcse, s);
+// })();
 
 var hint = document.getElementById('hint')
 var hintOverlay = document.getElementById('hintOverlay')
@@ -20,8 +20,8 @@ if(!hintShown){
   })
 }
 
-  hintOverlay.addEventListener('click', function () {
-    hintOverlay.style.display = 'none'
-    hintShown = false
-    console.log("wtf");
-  })
+  // hintOverlay.addEventListener('click', function () {
+  //   hintOverlay.style.display = 'none'
+  //   hintShown = false
+  //   console.log("wtf");
+  // })
